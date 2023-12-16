82ec2398ceb0d173b0c62eb4308dc4e8cd1e9ffb
AhmidouA <ahmedakkioui75@gmail.com>
2023-12-16 20:41:16 +0100

    Project Dark-theme Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useLocalStorage from 'use-local-storage';
+
+import './index.css'
+
 
 function App() {
+
+  //
+  const [theme, setTheme] = useLocalStorage( 'dark' , 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <div className='login'>
+        <h1> Login</h1>
+        <div className='container'>
+          <div className='top'>
+          <i className="fa-brands fa-google"></i>
+            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-square-twitter"></i>
+            <i className="fa-brands fa-apple"></i>
+          </div>
+          <p className='divider'><span>Or</span></p>
+          <form>
+            <label>E-mail</label>
+            <input type='email' placeholder='Enter your email' />
+            <label>Password</label>
+            <input type='password' placeholder='Enter your password' />
+            <div className='remember'>
+              <input type='checkbox' />
+              <p>Remember Me</p>
+            </div>
+            <button>Log In</button>
+          </form>
+          <div className='bottom'>
+            <p>Forget your password?</p>
+            <a href='/'>Reset Password</a>
+          </div>
+          <p className='create'>Create Account</p>
+        </div>
+        <div className='theme-toggle'>
+          <h2>Light Theme</h2>
+          <i onClick={switchTheme} className='fas fa-toggle-on'></i>
+        </div>
+      </div>
     </div>
   );
 }
